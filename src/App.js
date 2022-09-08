65af7c357c09e24d51396e6d347b751f40eadf2e
lanthien943000 <hongnhat24022000@gmail.com>
2022-09-08 14:36:20 +0700

    add api file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { BASE_URL } from "./app/config";
+import apiService from "./app/apiService";
 
 function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await apiService.get("/jobs");
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+    console.log(BASE_URL);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Axios</h1>
     </div>
   );
 }
